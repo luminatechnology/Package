de52877ddaca8da2f4bf2c4aedfbc36b2ff8628e
WayneLin <wayne.lin@lumina-tech.com>
2022-07-13 09:14:13 +0800

    1.新增Package+專案 2.新增TO Type的SOOrder Selector

---
+++ PackAgeCustomization/PackAgeCustomization/DAC/SOOrderKvExt.cs
@@ -0,0 +1,44 @@
+﻿using PX.Data;
+using PX.Data.BQL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackAgeCustomization.DAC
+{
+    [PXCacheName("SOOrder Attributes")]
+    [Serializable]
+    public class SOOrderKvExt : IBqlTable
+    {
+        public abstract class recordID : BqlGuid.Field<recordID> { }
+        [PXDBGuid(IsKey = true)]
+        public Guid? RecordID { get; set; }
+
+        public abstract class fieldName : BqlString.Field<fieldName> { }
+        [PXDBString(50, IsKey = true)]
+        [PXUIField(DisplayName = "Name")]
+        public string FieldName { get; set; }
+
+        public abstract class valueNumeric : BqlDecimal.Field<valueNumeric> { }
+        [PXDBDecimal(8)]
+        [PXUIField(DisplayName = "Value Numeric")]
+        public decimal? ValueNumeric { get; set; }
+
+        public abstract class valueDate : BqlDateTime.Field<valueDate> { }
+        [PXDBDate]
+        [PXUIField(DisplayName = "Value Date")]
+        public DateTime? ValueDate { get; set; }
+
+        public abstract class valueString : BqlString.Field<valueString> { }
+        [PXDBString(256)]
+        [PXUIField(DisplayName = "Value String")]
+        public string ValueString { get; set; }
+
+        public abstract class valueText : BqlString.Field<valueText> { }
+        [PXDBString]
+        [PXUIField(DisplayName = "Value Text")]
+        public string ValueText { get; set; }
+    }
+}
---
+++ PackAgeCustomization/PackAgeCustomization/DAC_Inheritance/LUMSOOrder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PackAgeCustomization.DAC;
+using PX.Data;
+using PX.Data.BQL.Fluent;
+using PX.Objects.CR;
+using PX.Objects.SO;
+
+namespace PackAgeCustomization.DAC_Inheritance
+{
+    public class LUMSOOrder : SOOrder
+    {
+
+        [PXSelector(typeof(SelectFrom<SOOrder>
+                          .LeftJoin<SOOrderKvExt>.On<SOOrder.noteID.IsEqual<SOOrderKvExt.recordID>>
+                          .LeftJoin<BAccount>.On<BAccount.acctCD.IsEqual<SOOrderKvExt.valueString>>
+                          .Where<SOOrder.orderType.IsEqual<SOType_TOAttr>>
+                          .SearchFor<SOOrder.orderNbr>),
+            typeof(SOOrder.orderType),
+            typeof(SOOrder.orderNbr),
+            typeof(SOOrder.status),
+            typeof(SOOrder.orderDate),
+            typeof(SOOrder.orderDesc),
+            typeof(BAccount.acctName),
+           SubstituteKey = typeof(SOOrder.orderNbr))]
+        public override String OrderNbr { get; set; }
+    }
+
+    public class SOType_TOAttr : PX.Data.BQL.BqlString.Constant<SOType_TOAttr>
+    {
+        public SOType_TOAttr() : base("TO") { }
+    }
+
+    public class CUSTOMERAttr : PX.Data.BQL.BqlString.Constant<CUSTOMERAttr>
+    {
+        public CUSTOMERAttr() : base("AttributeCUSTOMER") { }
+    }
+}
