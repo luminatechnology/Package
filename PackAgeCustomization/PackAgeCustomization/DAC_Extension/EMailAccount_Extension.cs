021f525a46dccfe36c3c3586a12dd2c58c543f48
WayneLin <wayne.lin@lumina-tech.com>
2022-07-15 09:45:59 +0800

    1.更改Email Account 欄位

---
+++ PackAgeCustomization/PackAgeCustomization/DAC_Extension/EMailAccount_Extension.cs
@@ -0,0 +1,18 @@
+﻿using PX.Data;
+using PX.SM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackAgeCustomization.DAC_Extension
+{
+    public class EMailAccount_Extension : PXCacheExtension<EMailAccount>
+    {
+        [PXDBString(100, IsUnicode = true)]
+        [PXUIField(DisplayName = "Sender Name")]
+        [PXMergeAttributes(Method = MergeMethod.Replace)]
+        public virtual string AccountDisplayName { get; set; }
+    }
+}
