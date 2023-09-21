4b3a3ca6bca4b558dc6fa4ffdc00a939e459ef40
Administrator <Administrator@EC2AMAZ-DVS6HJL>
2023-09-21 15:39:25 +0800

    Add a new field to be the condition.

---
+++ PackAgeCustomization/PackAgeCustomization/DAC_Extension/SOOrderTypeExtensions.cs
@@ -0,0 +1,14 @@
+﻿using PX.Data;
+
+namespace PX.Objects.SO
+{
+    public class SOOrderTypeExt : PXCacheExtension<PX.Objects.SO.SOOrderType>
+    {
+        #region UsrUseDiscountAcctFromSalesAcct
+        [PXDBBool]
+        [PXUIField(DisplayName = "Use Discount Account from Sales Account")]
+        public virtual bool? UsrUseDiscountAcctFromSalesAcct { get; set; }
+        public abstract class usrUseDiscountAcctFromSalesAcct : PX.Data.BQL.BqlBool.Field<usrUseDiscountAcctFromSalesAcct> { }
+        #endregion
+    }
+}
---
+++ PackAgeCustomization/PackAgeCustomization/Graph_Extensions/SOOrderTypeMaint.cs
@@ -0,0 +1,75 @@
+﻿using PX.Data;
+
+namespace PX.Objects.SO
+{
+    public class SOOrderTypeMaint_Extension : PXGraphExtension<SOOrderTypeMaint>
+    {
+        #region Event Handlers
+        protected void _(Events.RowSelected<SOOrderType> e, PXRowSelected baseHandler)
+        {
+            baseHandler?.Invoke(e.Cache, e.Args);
+
+            SOOrderType ordertype = e.Row;
+
+            if (ordertype != null) 
+            {
+                PXUIFieldAttribute.SetEnabled<SOOrderTypeExt.usrUseDiscountAcctFromSalesAcct>(e.Cache, ordertype, ordertype.PostLineDiscSeparately == true);
+
+                if (ordertype.Behavior == SOBehavior.BL)
+                {
+                    e.Cache.Adjust<PXUIFieldAttribute>(ordertype)
+                    //Order settings
+                    .For<SOOrderType.creditHoldEntry>(a =>
+                    {
+                        a.Visible = a.Enabled = false;
+                    })
+                    .SameFor<SOOrderType.daysToKeep>()
+                    .SameFor<SOOrderType.billSeparately>()
+                    .SameFor<SOOrderType.shipSeparately>()
+                    .SameFor<SOOrderType.calculateFreight>()
+                    .SameFor<SOOrderType.shipFullIfNegQtyAllowed>()
+                    .SameFor<SOOrderType.disableAutomaticDiscountCalculation>()
+                    .SameFor<SOOrderType.recalculateDiscOnPartialShipment>()
+                    .SameFor<SOOrderType.allowRefundBeforeReturn>()
+                    .SameFor<SOOrderType.copyNotes>()
+                    .SameFor<SOOrderType.copyFiles>()
+                    .SameFor<SOOrderType.copyLineNotesToShipment>()
+                    .SameFor<SOOrderType.copyLineFilesToShipment>()
+                    .SameFor<SOOrderType.copyLineNotesToInvoice>()
+                    .SameFor<SOOrderType.copyLineNotesToInvoiceOnlyNS>()
+                    .SameFor<SOOrderType.copyLineFilesToInvoice>()
+                    .SameFor<SOOrderType.copyLineFilesToInvoiceOnlyNS>()
+                    //Accounts Receipvable Settings
+                    .SameFor<SOOrderType.invoiceNumberingID>()
+                    .SameFor<SOOrderType.markInvoicePrinted>()
+                    .SameFor<SOOrderType.markInvoiceEmailed>()
+                    .SameFor<SOOrderType.invoiceHoldEntry>()
+                    .SameFor<SOOrderType.useCuryRateFromSO>()
+                    //Posting Settings
+                    .SameFor<SOOrderType.salesAcctDefault>()
+                    .SameFor<SOOrderType.salesSubMask>()
+                    .SameFor<SOOrderType.freightAcctID>()
+                    .SameFor<SOOrderType.freightAcctDefault>()
+                    .SameFor<SOOrderType.freightSubID>()
+                    .SameFor<SOOrderType.freightSubMask>()
+                    .SameFor<SOOrderType.discountAcctID>()
+                    .SameFor<SOOrderType.discAcctDefault>()
+                    .SameFor<SOOrderType.discountSubID>()
+                    .SameFor<SOOrderType.discSubMask>()
+                    .SameFor<SOOrderType.postLineDiscSeparately>()
+                    .SameFor<SOOrderType.useDiscountSubFromSalesSub>()
+                    // New custom field
+                    .SameFor<SOOrderTypeExt.usrUseDiscountAcctFromSalesAcct>()
+                    .SameFor<SOOrderType.autoWriteOff>()
+                    .SameFor<SOOrderType.useShippedNotInvoiced>()
+                    .SameFor<SOOrderType.shippedNotInvoicedAcctID>()
+                    .SameFor<SOOrderType.shippedNotInvoicedSubID>()
+                    //Intercompany Posting Settings
+                    .SameFor<SOOrderType.intercompanySalesAcctDefault>()
+                    .SameFor<SOOrderType.intercompanyCOGSAcctDefault>();
+                }
+            }
+        }
+        #endregion
+    }
+}
