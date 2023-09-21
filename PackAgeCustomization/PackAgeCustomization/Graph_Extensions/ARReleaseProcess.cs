e1fac44898c418362d678fa8f6a1250db10cec76
Administrator <Administrator@EC2AMAZ-DVS6HJL>
2023-09-21 15:41:14 +0800

    Added a new extension logic to override journal transaction discount account from sales order.

---
+++ PackAgeCustomization/PackAgeCustomization/Graph_Extensions/ARReleaseProcess.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using PX.Data;
+using PX.Objects.SO;
+
+namespace PX.Objects.AR
+{
+    public class ARReleaseProcess_Extension : PXGraphExtension<ARReleaseProcess>
+    {
+        #region Override Methods
+        [PXOverride]
+        public virtual void ProcessInvoiceDetailDiscount(ARRegister doc, Customer customer, ARTran tran, SOOrderType sotype, ARInvoice ardoc,
+                                                         Action<ARRegister, Customer, ARTran, SOOrderType, ARInvoice> baseMethod)
+        {
+            baseMethod(doc, customer, tran, sotype, ardoc);
+
+            var curTran = Base.ARTran_TranType_RefNbr.Current;
+
+            if (sotype.GetExtension<SOOrderTypeExt>().UsrUseDiscountAcctFromSalesAcct == true && curTran != null)
+            {
+                Base.ARTran_TranType_RefNbr.SetValueExt<ARTran.accountID>(curTran, SOLine.PK.Find(Base, curTran.SOOrderType, curTran.SOOrderNbr, curTran.SOOrderLineNbr)?.SalesAcctID);
+                Base.ARTran_TranType_RefNbr.Update(curTran);
+            }
+        }
+        #endregion
+    }
+}
